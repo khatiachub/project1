5ec83f7b38fcc491e1a334c4e6834d6f41828435
khatiachub <101997849+khatiachub@users.noreply.github.com>
2023-02-16 23:14:12 +0400

    deploy

--- rio/src/index.js
+++ rio/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import {HashRouter, Route,Routes} from 'react-router-dom';
 import Home from './components/Home';
 import Apartament from './components/Apartament';
 import Contact from './components/Contact';
@@ -10,22 +10,24 @@ import RoomDescription from './RoomDescription';
 import Root from './Root';
 import './components/fonts/Fonts.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const router=createBrowserRouter(
-  createRoutesFromElements(
-      <Route path='/' element={<Root/>}>
-      <Route index element={<Home/>}/>
-      <Route path='/Our Apartament'>
-         <Route index={true}  element={<Apartament/>}/>
-         <Route  path='/Our Apartament/:id' element={<RoomDescription/>}/>
-      </Route>
-      <Route path='/Rio' element={<Rio/>}/>
-      <Route path='/Contact'element={<Contact/>}/>
-      <Route path='/Book' element={<Book/>}/>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+function App(){
+  return(
+    <HashRouter>
+      <Routes>
+    <Route path='/' element={<Root/>}>
+    <Route index element={<Home/>}/>
+    <Route path='/Our Apartament'>
+       <Route index={true}  element={<Apartament/>}/>
+       <Route  path='/Our Apartament/:id' element={<RoomDescription/>}/>
     </Route>
+    <Route path='/Rio' element={<Rio/>}/>
+    <Route path='/Contact'element={<Contact/>}/>
+    <Route path='/Book' element={<Book/>}/>
+  </Route>
+  </Routes>
+  </HashRouter>
   )
-)
-root.render(
-  <RouterProvider router={router}/>
-);
+}
+root.render(<App/>)
